c8278bff4433391be70429f7f2e2cc273c5d5e22
mahimaa333 <60659758+mahimaa333@users.noreply.github.com>
2021-05-06 00:12:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+$('.slides').slick({
+    slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows:true,
+  focusOnSelect:true,
+  
+}
+);
+
+$('.items').slick({
+    slidesToShow: 2,
+  slidesToScroll: 2,
+  arrows:true,
+  focusOnSelect:true,
+  dots:true,
+}
+);
---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "300px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+  
